43c672e1d3441c600a30aa76357ab875f8329c95
João Vitor Lira Bezerra <vitor.lira98@gmail.com>
2020-01-21 00:24:53 -0300

    Adicionando repositórios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
+
+import api from '../../services/api';
 
 export default function Main() {
-  return <Title>Hello World</Title>;
+  const [repo, setRepo] = useState('');
+  const [load, setLoad] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoad(true);
+    const response = await api.get(`/repos/${repo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+    setRepo([...repo, data]);
+    setRepo('');
+    setLoad(false);
+  }
+
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositorios
+      </h1>
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar repositório"
+          value={repo}
+          onChange={e => setRepo(e.target.value)}
+        />
+
+        <SubmitButton loading={load}>
+          {load ? (
+            <FaSpinner color="#fff" size={14} />
+          ) : (
+            <FaPlus color="#FFF" size={14} />
+          )}
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
